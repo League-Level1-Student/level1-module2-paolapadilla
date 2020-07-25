fbc7b0964ebb10b6136f2a5a2360f42588c21e8b
root <paolapadilla061@gmail.com>
2020-07-25 18:34:17 +0000

    working on Tea Party.

---
+++ src/_07_tea_party/TeaParty.java
@@ -0,0 +1,23 @@
+package _07_tea_party;
+
+public class TeaParty {
+	
+	public String welcome(String name, boolean isWoman == false) {
+		
+	      if(isKnighted == true && isWoman == false) {
+	    	  
+	    	  return "Hello Sir" + name;
+	      } else if (isWoman == false && isKnighted == false) {
+	    	  
+	    	  return "Hello Mr. " + name;
+	      } else if (isKnighted == true && isWoman == true) {
+	    	  
+	    	  return "Hello Lady" + name;
+	      } else if (isWoman == true && isKnighted == false) {
+	    	  
+	    	  return "Hello Ms." + name;
+	      }else {
+	    	  return name;
+		}
+
+}
---
+++ src/_07_tea_party/TeaPartyTest.java
@@ -0,0 +1,10 @@
+package _07_tea_party;
+
+public class TeaPartyTest {
+
+	public static void main(String[] args) {
+TeaParty teaparty = new TeaParty();
+assertEquals()
+	}
+
+}
