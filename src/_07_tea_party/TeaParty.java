fb0b8a7ad2f16bbd5b7204592f6268d56e5c887e
Paola Padilla <paolapadilla061@gmail.com>
2020-08-01 18:33:10 +0000

    I'm working on Tea party

--- src/_07_tea_party/TeaParty.java
+++ src/_07_tea_party/TeaParty.java
@@ -1,23 +1,23 @@
 package _07_tea_party;
 
 public class TeaParty {
-	
-	public String welcome(String name, boolean isWoman == false) {
-		
-	      if(isKnighted == true && isWoman == false) {
-	    	  
-	    	  return "Hello Sir" + name;
-	      } else if (isWoman == false && isKnighted == false) {
-	    	  
-	    	  return "Hello Mr. " + name;
-	      } else if (isKnighted == true && isWoman == true) {
-	    	  
-	    	  return "Hello Lady" + name;
-	      } else if (isWoman == true && isKnighted == false) {
-	    	  
-	    	  return "Hello Ms." + name;
-	      }else {
-	    	  return name;
-		}
 
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
+
+		if (isKnighted == true && isWoman == false) {
+
+			return "Hello Sir" + name;
+		} else if (isWoman == false && isKnighted == false) {
+
+			return "Hello Mr. " + name;
+		} else if (isKnighted == true && isWoman == true) {
+
+			return "Hello Lady" + name;
+		} else if (isWoman == true && isKnighted == false) {
+
+			return "Hello Ms." + name;
+		} else {
+			return name;
+		}
+	}
 }
--- src/_07_tea_party/TeaPartyTest.java
+++ src/_07_tea_party/TeaPartyTest.java
@@ -1,10 +1,40 @@
 package _07_tea_party;
 
-public class TeaPartyTest {
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-	public static void main(String[] args) {
-TeaParty teaparty = new TeaParty();
-assertEquals()
+public class TeaPartyTest {
+	
+
+	@Test
+	public void testIsWomanNotKnighted() {
+		TeaParty teaParty = new TeaParty();
+		String greeting = teaParty.welcome("null", false, false);
+		assertEquals("Hello Ms. Austen", greeting);
+	}
+
+	@Test
+	public void testIsManNotKnighted() {
+		TeaParty teaParty = new TeaParty();
+		String greeting = teaParty.welcome(null, false, false);
+		assertEquals("Hello Mr. Orwell", teaParty.welcome("Orwell", false, false));
+	}
+
+	@Test
+	public void testIsManKnighted() {
+		TeaParty teaParty = new TeaParty();
+		String greeting = teaParty.welcome(null, false, false);
+		assertEquals("Hello Sir Isaac Newton", teaParty.welcome("Isaac Newton", false, true));
+}
+	
+	@Test
+	public void testIsWomanKnighted() {
+		TeaParty teaParty = new TeaParty();
+		String greeting = teaParty.welcome(null, false, false);
+		assertEquals("Hello Lady Laura", teaParty.welcome("Laura", true, true));
 	}
 
 }
+    
+
+
