0ba196625def6338ebe7d68bfc555f13c6830e80
paola <paolapadilla061@gmail.com>
2020-08-29 18:20:57 +0000

    I finished Sea Creature.

---
+++ src/_02_sea_creature/SeaCreatureRunner.java
@@ -0,0 +1,16 @@
+package _02_sea_creature;
+
+public class SeaCreatureRunner {
+	
+	public static void main(String[] args) {
+		SeaCreature Creature = new SeaCreature("spongbob");
+		Creature.eat();
+		
+		SeaCreature Creature1 = new SeaCreature("patrick");
+		Creature1.eat();
+		
+		SeaCreature Creature2 = new SeaCreature("Squidward");
+		Creature2.eat();
+	}
+
+}
